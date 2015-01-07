fd4917338687df9e316b1476d4cfede2c81b22fb
ericrius1 <ericrius1@gmail.com>
2015-01-06 18:18:21 -0800

    min threejs

--- js/sunset.js
+++ js/sunset.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
 	var waterNormals, time, water, mirrorMesh;
 	var sun;
 	var oceanSize = 20000;
-	var sunRadius = 1000;
+	var sunRadius = 1100;
 	var sunStartHeight = sunRadius * 2;
 	var sunsetHeight = -sunRadius * 2;
+	var color
 
 	init()
 	animate()
@@ -15,20 +16,20 @@ $(document).ready(function() {
 
 
 		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-		camera.position.set(0, 30, 1000);
+		camera.position.set(0, 400, 1000);
 		scene = new THREE.Scene();
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		$('#canvas-container').prepend(renderer.domElement);
 
 
-		var sunGeo = new THREE.CircleGeometry(sunRadius, 32);
+		var sunGeo = new THREE.CircleGeometry(sunRadius, 64);
 		var sunMat = new THREE.MeshBasicMaterial({
 			color: 0xff0000
 		});
 		sun = new THREE.Mesh(sunGeo, sunMat);
 		sun.position.y = sunStartHeight;
-		sun.position.z = -oceanSize/3
+		sun.position.z = -oceanSize/2.5
 		scene.add(sun);
 
 		waterNormals = new THREE.ImageUtils.loadTexture('img/waternormals.jpg');
@@ -39,7 +40,7 @@ $(document).ready(function() {
 			textureHeight: 512,
 			waterNormals: waterNormals,
 			alpha: 1.0,
-			sunColor: 0xffffff,
+			sunColor: sun.material.color,
 			waterColor: 0x001e0f,
 			distortionScale: 50.0,
 		});
