4d81bfed1afd1df3e8c589e3a7b67fa54c4ddd1d
ericrius1 <ericrius1@gmail.com>
2015-01-06 17:28:39 -0800

    water

--- js/sunset.js
+++ js/sunset.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 	var scene, renderer, camera, w, h;
+	var waterNormals, time, water, mirrorMesh;
 
 	init()
 	animate()
@@ -9,19 +10,45 @@ $(document).ready(function(){
 		w = window.innerWidth;
 		h = window.innerHeight
 		camera = new THREE.PerspectiveCamera(45, w / h, 1, 10000);
+		camera.position.set( 2000, 750, 2000 );
 		scene = new THREE.Scene();
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize(w, h);
 		$('#canvas-container').prepend(renderer.domElement);
 
 
+
 		var mesh = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10));
 		mesh.position.z = -30;
 		scene.add(mesh);
+
+		waterNormals = new THREE.ImageUtils.loadTexture('img/waternormals.jpg');
+		waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+
+		water = new THREE.Water(renderer, camera, scene, {
+			textureWidth: 512,
+			textureHeight: 512,
+			waterNormals: waterNormals,
+			alpha: 1.0,
+			sunColor: 0xffffff,
+			waterColor: 0x001e0f,
+			distortionScale: 50.0,
+		});
+
+		mirrorMesh = new THREE.Mesh(
+			new THREE.PlaneBufferGeometry(5000, 5000),
+			water.material
+		);
+
+		mirrorMesh.add(water);
+		mirrorMesh.rotation.x = -Math.PI * 0.5;
+		scene.add(mirrorMesh);
 	}
 
 	function animate() {
 		requestAnimationFrame(animate);
+		water.material.uniforms.time.value += 1.0 / 60.0;
+		water.render();
 		renderer.render(scene, camera);
 
 	}
